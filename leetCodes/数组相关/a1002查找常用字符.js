c3efe95fb5d19c9a313e48ca62027c5f51a76100
maoweiwei1995 <445865910@qq.com>
2019-09-25 21:27:46 +0800

    200道纪念

---
+++ leetCodes/数组相关/a1002查找常用字符.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} A
+ * @return {string[]}
+ */
+var commonChars = function(A) {
+    let len = A.length
+    let ans = []
+    for (let j = 0; j < A[0].length; j++){
+        let temp = A[0][j]
+        for (let i = 1; i < A; i++){
+            if ()
+        }
+        ans.push(temp)
+    }
+};
---
+++ leetCodes/递归/b779第K个语法符号.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} N
+ * @param {number} K
+ * @return {number}
+ */
+var kthGrammar = function(N, K) {
+    let str = '0'  
+    if(N > 0) {
+        digui(N-1, K)
+    }
+};
+function digui(str, n, k){
+    let temp = 1
+    for (let i = 0; i < n; i++)
+    let str2 = parseInt(str, 2) ^ 
+    if (n > 0){
+        digui(str, n-1, k)
+    }
+}
